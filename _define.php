fc5294f2a80d92329702a966cd36562618ffbaa0
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:10 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,22 +10,25 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "hscroll",                      // Name
-    "Horizontal scrollbar",         // Description
-    "Franck Paul and contributors", // Author
+    'hscroll',                      // Name
+    'Horizontal scrollbar',         // Description
+    'Franck Paul and contributors', // Author
     '0.3',                          // Version
     [
         'requires'    => [['core', '2.17']],
         'permissions' => 'admin',
         'type'        => 'plugin',
-        'details'     => 'https://open-time.net/?q=hscroll',       // Details URL
-        'support'     => 'https://github.com/franck-paul/hScroll', // Support URL
         'settings'    => [
             'blog' => '#params.hscroll'
-        ]
+        ],
+
+        'details'    => 'https://open-time.net/?q=hscroll',       // Details URL
+        'support'    => 'https://github.com/franck-paul/hscroll', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/hscroll/main/dcstore.xml'
     ]
 );
