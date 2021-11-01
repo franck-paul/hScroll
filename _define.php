80e06097c2d280c3880b5b815d094c9b0736ff5b
franck <carnet.franck.paul@gmail.com>
2021-11-01 09:31:06 +0100

    Update default branch in _define.php for alternative repository

--- _define.php
+++ _define.php
@@ -29,6 +29,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=hscroll',       // Details URL
         'support'    => 'https://github.com/franck-paul/hscroll', // Support URL
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/hscroll/main/dcstore.xml'
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/hscroll/master/dcstore.xml'
     ]
 );
