d11e051376aac754722d95d234d68cf8d9da3a48
franck <carnet.franck.paul@gmail.com>
2022-12-26 18:26:27 +0100

    Use Dotclear\Plugins\hScroll namespace

--- _admin.php
+++ _admin.php
@@ -11,7 +11,10 @@
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace plugins\hscroll;
+namespace Dotclear\Plugins\hScroll;
+
+use dcCore;
+use form;
 
 if (!defined('DC_CONTEXT_ADMIN')) {
     return;
@@ -20,8 +23,10 @@ if (!defined('DC_CONTEXT_ADMIN')) {
 // dead but useful code, in order to have translations
 __('hscroll') . __('Horizontal scrollbar');
 
-\dcCore::app()->addBehavior('adminBlogPreferencesFormV2', [__NAMESPACE__ . '\hscrollBehaviors', 'adminBlogPreferencesForm']);
-\dcCore::app()->addBehavior('adminBeforeBlogSettingsUpdate', [__NAMESPACE__ . '\hscrollBehaviors', 'adminBeforeBlogSettingsUpdate']);
+dcCore::app()->addBehaviors([
+    'adminBlogPreferencesFormV2'    => [__NAMESPACE__ . '\hscrollBehaviors', 'adminBlogPreferencesForm'],
+    'adminBeforeBlogSettingsUpdate' => [__NAMESPACE__ . '\hscrollBehaviors', 'adminBeforeBlogSettingsUpdate'],
+]);
 
 class hscrollBehaviors
 {
@@ -33,35 +38,34 @@ class hscrollBehaviors
             __('Bottom') => 'bottom',
         ];
 
-        $settings->addNameSpace('hscroll');
         $color = ($settings->hscroll->color ?: '#e9573f');
 
         echo
         '<div class="fieldset"><h4 id="hscroll">hScroll</h4>' .
 
         '<p><label class="classic">' .
-        \form::checkbox('hscroll_enabled', '1', $settings->hscroll->enabled) .
+        form::checkbox('hscroll_enabled', '1', $settings->hscroll->enabled) .
         __('Enable horizontal scrollbar') . '</label></p>' .
 
         '<h5>' . __('Options') . '</h5>' .
 
         '<p><label for="hscroll_position" class="classic">' . __('Position:') . '</label> ' .
-        \form::combo('hscroll_position', $styles, $settings->hscroll->position) .
+        form::combo('hscroll_position', $styles, $settings->hscroll->position) .
         '</p>' .
 
         '<p><label for="hscroll_offset" class="classic">' . __('Offset position (in pixels):') . '</label> ' .
-        \form::number('hscroll_offset', ['default' => $settings->hscroll->offset]) .
+        form::number('hscroll_offset', ['default' => $settings->hscroll->offset]) .
         '</p>' .
 
         '<p><label for="hscroll_color" class="classic">' . __('Scrollbar color:') . '</label> ' .
-        \form::color('hscroll_color', ['default' => $color]) . '</p>' .
+        form::color('hscroll_color', ['default' => $color]) . '</p>' .
 
         '<p><label for="hscroll_shadow" class="classic">' .
-        \form::checkbox('hscroll_shadow', '1', $settings->hscroll->shadow) .
+        form::checkbox('hscroll_shadow', '1', $settings->hscroll->shadow) .
         __('Add shadow to the scrollbar') . '</label>' . '</p>' .
 
         '<p><label for="hscroll_single" class="classic">' .
-        \form::checkbox('hscroll_single', '1', $settings->hscroll->single) .
+        form::checkbox('hscroll_single', '1', $settings->hscroll->single) .
         __('Activate only in single entry context') . '</label>' . '</p>' .
 
             '</div>';
@@ -69,7 +73,6 @@ class hscrollBehaviors
 
     public static function adminBeforeBlogSettingsUpdate($settings)
     {
-        $settings->addNameSpace('hscroll');
         $settings->hscroll->put('enabled', !empty($_POST['hscroll_enabled']), 'boolean');
         $settings->hscroll->put('position', $_POST['hscroll_position']);
         $settings->hscroll->put('offset', (int) $_POST['hscroll_offset']);
--- _public.php
+++ _public.php
@@ -11,74 +11,77 @@
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace plugins\hscroll;
+namespace Dotclear\Plugins\hScroll;
+
+use dcCore;
+use dcUtils;
 
 if (!defined('DC_RC_PATH')) {
     return;
 }
 
-\dcCore::app()->addBehavior('publicHeadContent', [__NAMESPACE__ . '\hscrollPublic', 'publicHeadContent']);
-\dcCore::app()->addBehavior('publicFooterContent', [__NAMESPACE__ . '\hscrollPublic', 'publicFooterContent']);
+dcCore::app()->addBehaviors([
+    'publicHeadContent'   => [__NAMESPACE__ . '\hscrollPublic', 'publicHeadContent'],
+    'publicFooterContent' => [__NAMESPACE__ . '\hscrollPublic', 'publicFooterContent'],
+]);
 
 class hscrollPublic
 {
     public static function publicHeadContent()
     {
-        \dcCore::app()->blog->settings->addNameSpace('hscroll');
-        if (!\dcCore::app()->blog->settings->hscroll->enabled) {
+        if (!dcCore::app()->blog->settings->hscroll->enabled) {
             return;
         }
 
-        if (\dcCore::app()->blog->settings->hscroll->single) {
+        if (dcCore::app()->blog->settings->hscroll->single) {
             // Single mode only, check if post/page context
             $urlTypes = ['post'];
-            if (\dcCore::app()->plugins->moduleExists('pages')) {
+            if (dcCore::app()->plugins->moduleExists('pages')) {
                 $urlTypes[] = 'page';
             }
-            if (!in_array(\dcCore::app()->url->type, $urlTypes)) {
+            if (!in_array(dcCore::app()->url->type, $urlTypes)) {
                 return;
             }
         }
 
-        $position = \dcCore::app()->blog->settings->hscroll->position;
+        $position = dcCore::app()->blog->settings->hscroll->position;
         if (!in_array($position, ['top', 'bottom', 'user'])) {
             $position = 'top';
         }
-        $offset = (int) \dcCore::app()->blog->settings->hscroll->offset;
+        $offset = (int) dcCore::app()->blog->settings->hscroll->offset;
 
-        echo \dcUtils::jsJson('hscroll', [
-            'color'  => (\dcCore::app()->blog->settings->hscroll->color ?: '#e9573f'),
+        echo dcUtils::jsJson('hscroll', [
+            'color'  => (dcCore::app()->blog->settings->hscroll->color ?: '#e9573f'),
             'top'    => ($position == 'top' ? "$offset" . 'px' : 'unset'),
             'bottom' => ($position == 'bottom' ? "$offset" . 'px' : 'unset'),
-            'shadow' => (\dcCore::app()->blog->settings->hscroll->shadow ? '1' : '0'),
+            'shadow' => (dcCore::app()->blog->settings->hscroll->shadow ? '1' : '0'),
         ]);
 
         echo
-        \dcUtils::jsModuleLoad('util.js') .
-        \dcUtils::jsModuleLoad('hScroll/js/cssvar.js') .
-        \dcUtils::cssModuleLoad('hScroll/css/hscroll.css');
+        dcUtils::jsModuleLoad('util.js') .
+        dcUtils::jsModuleLoad('hScroll/js/cssvar.js') .
+        dcUtils::cssModuleLoad('hScroll/css/hscroll.css');
     }
 
     public static function publicFooterContent()
     {
-        \dcCore::app()->blog->settings->addNameSpace('hscroll');
-        if (!\dcCore::app()->blog->settings->hscroll->enabled) {
+        if (!dcCore::app()->blog->settings->hscroll->enabled) {
             return;
         }
 
-        if (\dcCore::app()->blog->settings->hscroll->single) {
+        if (dcCore::app()->blog->settings->hscroll->single) {
             // Single mode only, check if post/page context
             $urlTypes = ['post'];
-            if (\dcCore::app()->plugins->moduleExists('pages')) {
+            if (dcCore::app()->plugins->moduleExists('pages')) {
                 $urlTypes[] = 'page';
             }
-            if (!in_array(\dcCore::app()->url->type, $urlTypes)) {
+            if (!in_array(dcCore::app()->url->type, $urlTypes)) {
                 return;
             }
         }
 
         echo
         '<div id="hscroll-bar"><div id="hscroll-bar-inner"></div></div>' . "\n" .
-        \dcUtils::jsModuleLoad('hScroll/js/hscroll.js');
+        dcUtils::jsModuleLoad('hScroll/js/hscroll.js');
     }
 }
