3cd666ea10a8ca79cce03fb5d06e64dee33bdd77
franck <carnet.franck.paul@gmail.com>
2023-08-25 12:02:48 +0200

    New release 3.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'hscroll',
     'Horizontal scrollbar',
     'Franck Paul and contributors',
-    '2.1.2',
+    '3.0',
     [
         'requires'    => [['core', '2.27'], ['php', '8.1']],
         'permissions' => dcCore::app()->auth->makePermissions([
