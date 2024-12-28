c062deba61c29ed33aa75f29a26b1b6d2ddfdf38
franck <carnet.franck.paul@gmail.com>
2024-12-28 06:10:48 +0100

    PHP Code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief hScroll, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('hscroll') . __('Horizontal scrollbar');
+        __('hscroll');
+        __('Horizontal scrollbar');
 
         return self::status(My::checkContext(My::BACKEND));
     }
