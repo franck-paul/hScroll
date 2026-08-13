5d1840adc1284ff12874553f36f2b65e5c17a589
franck <contact@open-time.net>
2026-08-13 16:42:04 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,23 +11,27 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'hScroll',
-    'Horizontal or vertical reading scrollbar',
-    'Franck Paul and contributors',
-    '6.0',
-    [
-        'date'        => '2026-08-03T10:00:16+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => [
-            'blog' => '#params.hscroll',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=hscroll',
-        'support'    => 'https://github.com/franck-paul/hscroll',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/hscroll/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'hScroll',
+        'Horizontal or vertical reading scrollbar',
+        'Franck Paul and contributors',
+        '6.0',
+        [
+            'date'        => '2026-08-03T10:00:16+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => [
+                'blog' => '#params.hscroll',
+            ],
+
+            'details'    => 'https://open-time.net/?q=hscroll',
+            'support'    => 'https://github.com/franck-paul/hscroll',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/hscroll/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
